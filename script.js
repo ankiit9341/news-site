38c0543b865936f4fdecb4ffb5a560a245e1494e
Ankit Kumar Yadav <itsankit9341@gmail.com>
2024-06-30 21:35:12 +0530

    Initial commit: Add favicon images, logo, and initial project files

---
+++ script.js
@@ -0,0 +1,66 @@
+const API_KEY = "2b1bb0d5cb2941b598e8b0975763e2cc";
+const url = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener('load', ()=> fetchNews("india"));
+
+function reload(){
+    fetchNews("viral news");
+}
+
+async function fetchNews(query) {
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data = await res.json();
+    bindData(data.articles);
+}
+ function bindData(articles) {
+    const cardsContainer = document.getElementById('card-container');
+    const cardsTamplate = document.getElementById('template-news-card');
+
+    cardsContainer.innerHTML = "";
+
+    articles.forEach(article => {
+        if(!article.urlToImage) return;
+        const cardClone = cardsTamplate.content.cloneNode(true);
+        fillDataInCard(cardClone,article);
+        cardsContainer.appendChild(cardClone);
+    });
+
+    function fillDataInCard(cardClone,article){
+        const newsImg = cardClone.querySelector('#news-img');
+        const newsTitle = cardClone.querySelector('#news-title');
+        const newsSrc = cardClone.querySelector('#news-src');
+        const newsDesc = cardClone.querySelector('#news-dis');
+
+        newsImg.src = article.urlToImage;
+        newsTitle.innerHTML = article.title;
+        newsDesc.innerHTML = article.description;
+
+        const date = new Date(article.publishedAt).toLocaleString('eg-US',{
+            timeZone: "Asia/Jakarta"
+        });
+        newsSrc.innerHTML = `${article.source.name} . ${date}`;
+
+        cardClone.firstElementChild.addEventListener("click", ()=>{
+            window.open(article.url,"_blank");
+        })
+    }
+}
+let currElement = null;
+function onclickNav(id){
+    fetchNews(id);
+    const navItem = document.getElementById(id);
+    currElement?.classList.remove("active");
+    currElement = navItem;
+    currElement.classList.add("active");
+}
+
+const searchBtn = document.getElementById('search-btn');
+const searchText = document.getElementById('search-text');
+
+searchBtn.addEventListener('click',()=>{
+    const query = searchText.value;
+    if(!query) return;
+    fetchNews(query);
+    currElement?.classList.remove("active");
+    currElement = null;
+})
